351f96fdf24d2aa3d00d4c9ed17a918eecd08d31
Nagaraju Sangam <nagaraju.sangam@Nagarajus-MBP.Dlink>
2022-04-12 11:25:21 +0530

    US1234:cleanup

--- src/App.js
+++ src/App.js
@@ -3,21 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin:"50px", fontSize: "20px"}}>
+       React Sample Application for deploymenty
     </div>
   );
 }
