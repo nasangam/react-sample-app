7c427317f2b7c51598532b22a5422b32e4e900ae
Nagaraju Sangam <nagaraju.sangam@Nagarajus-MBP.Dlink>
2022-04-12 12:16:37 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import logo from './logo.svg';
 import "./App.css";
 
 function App() {
+  console.log(process.env.NODE_ENV);
   return (
     <div className="App" style={{margin:"50px", fontSize: "20px"}}>
-       React Sample Application for deploymenty
+       <div>React Sample Application for deployment</div><hr></hr>
+       <div>Environment: {process.env.NODE_ENV} </div>
+       <div>REACT_APP_API_KEY: {process.env.REACT_APP_API_KEY}</div>
+       <div>REACT_APP_API_BASE: {process.env.REACT_APP_API_BASE}</div>
+       <div>MY_VAR: {process.env.MY_VAR}</div>
     </div>
   );
 }
